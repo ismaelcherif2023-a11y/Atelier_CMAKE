8576575a0aa99ec58d3c9beabc069efbc549d3a2
ismaelcherif2023-a11y <ismaelcherif2023@gmail.com>
2026-03-02 10:55:18 +0000

    Ajout déclaration _car dans calculator.h

--- include/calculator.h
+++ include/calculator.h
@@ -1,4 +1,10 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
 double _add(double a, double b);
 double _sub(double a, double b);
 double _mul(double a, double b);
 double _div(double a, double b);
+double _car(double a);
+
+#endif
